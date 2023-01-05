42a61dd3ebdfbaf48d747de4e0780f3800ce2de3
wn <wn@wn.com>
2023-01-05 22:01:55 +0800

    第二次提交！

--- src/main/java/com/wn/git/GitTest.java
+++ src/main/java/com/wn/git/GitTest.java
@@ -4,5 +4,6 @@ package com.wn.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git！");
+        System.out.println("hello git2！");
     }
 }
